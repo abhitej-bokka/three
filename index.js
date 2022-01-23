1111c98ddbb229efc1d3b9f1d08571080838dea4
Abhitej Bokka <71954158+abhitej-bokka@users.noreply.github.com>
2022-01-23 16:52:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -111,6 +111,30 @@ torusKnot.position.z = 1;
 
 // scroll Animation
 
+
+//Added by Abhitej - 1/23/22
+
+let light1, light2, light3, light4;
+const clock = new THREE.Clock();
+
+const sphere = new THREE.SphereGeometry( 0.5, 16, 8 );
+
+        light1 = new THREE.PointLight( 0xff0040, 2, 50 );
+				light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
+				scene.add( light1 );
+
+				light2 = new THREE.PointLight( 0x0040ff, 2, 50 );
+				light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
+				scene.add( light2 );
+
+				light3 = new THREE.PointLight( 0x80ff80, 2, 50 );
+				light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
+				scene.add( light3 );
+
+				light4 = new THREE.PointLight( 0xffaa00, 2, 50 );
+				light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
+				scene.add( light4 );
+
 function moveCamera() {
   // top prop here shows how far we are from the top of the webpage
   const t = document.body.getBoundingClientRect().top;
@@ -147,6 +171,28 @@ function animate() {
   torusKnot.rotation.z += 0.005; // rotation on z axis
 
   moon.rotation.x += 0.005;
+  
+  
+  const time = Date.now() * 0.0005;
+				const delta = clock.getDelta();
+
+				if ( object ) object.rotation.y -= 0.5 * delta;
+
+				light1.position.x = Math.sin( time * 0.7 ) * 30;
+				light1.position.y = Math.cos( time * 0.5 ) * 40;
+				light1.position.z = Math.cos( time * 0.3 ) * 30;
+
+				light2.position.x = Math.cos( time * 0.3 ) * 30;
+				light2.position.y = Math.sin( time * 0.5 ) * 40;
+				light2.position.z = Math.sin( time * 0.7 ) * 30;
+
+				light3.position.x = Math.sin( time * 0.7 ) * 30;
+				light3.position.y = Math.cos( time * 0.3 ) * 40;
+				light3.position.z = Math.sin( time * 0.5 ) * 30;
+
+				light4.position.x = Math.sin( time * 0.3 ) * 30;
+				light4.position.y = Math.cos( time * 0.7 ) * 40;
+				light4.position.z = Math.sin( time * 0.5 ) * 30;
   //controls.update();
 
   renderer.render(scene, camera);
