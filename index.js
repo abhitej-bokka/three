2e379ca12a9e2e740efbcc8cd34965c174e35d38
Abhitej Bokka <71954158+abhitej-bokka@users.noreply.github.com>
2022-01-25 22:23:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -136,9 +136,9 @@ function animate() {
 
  const time = Date.now() * 0.0005;
 
- donut1.position.x += Math.sin(time * 0.7) * 1;
- donut1.position.y += Math.cos(time * 0.5) * 2;
- donut1.position.z += Math.cos(time * 0.3) * 1;
+ donut1.position.x += Math.sin(time * 0.7) * 5;
+ donut1.position.y += Math.cos(time * 0.5) * 5;
+ donut1.position.z += Math.cos(time * 0.3) * 5;
 
 
 
