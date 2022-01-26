ecbfb747bc1fc86899b1735458e617d0c2f82e62
Abhitej Bokka <71954158+abhitej-bokka@users.noreply.github.com>
2022-01-26 14:58:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ moon.position.setX(-10);
 //moon.position.set(-10,0,30);
 
 orbit.position.setX(-6);
-orbit.position.z = 30;
+orbit.position.z = -15;
 
 
 abhi.position.x = 17;
@@ -182,7 +182,7 @@ function animate() {
  donut2.rotation.y += 0.02; // roatation along y axis
  donut2.rotation.z += 0.04; // rotation on z axis
 
- /*
+ 
  orbit.position.x += Math.sin(time) * 0.5;
  orbit.position.y += Math.cos(time) * 0.5;
  orbit.position.z += Math.cos(time) * 0.5;
@@ -190,7 +190,7 @@ function animate() {
  orbit.rotation.x -= 0.008; // rotation along x axis
  orbit.rotation.y += 0.02; // roatation along y axis
  orbit.rotation.z -= 0.005; // rotation on z axis
- */
+ 
  //---------------------------------------------------
  
  torus.rotation.x += 0.01; // rotation along x axis
