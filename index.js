041ad415dffb2267eb7215aae45156e1daa83f67
Abhitej Bokka <71954158+abhitej-bokka@users.noreply.github.com>
2022-01-26 18:25:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -184,9 +184,9 @@ function animate() {
 
  theta += Math.PI/250;
  
- orbit.position.x += Math.sin(theta) * 0.08;
- orbit.position.y += Math.cos(theta) * 0.08;
- orbit.position.z += Math.cos(theta) * 0.08;
+ orbit.position.x += Math.sin(theta) * 0.04;
+ orbit.position.y += Math.cos(theta) * 0.04;
+ orbit.position.z += Math.cos(theta) * 0.04;
 
  orbit.rotation.x -= 0.008; // rotation along x axis
  orbit.rotation.y += 0.02; // roatation along y axis
