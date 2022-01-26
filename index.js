3bfa9716e7d8f99ce8e0c13c64ca5b4a8aae6643
Abhitej Bokka <71954158+abhitej-bokka@users.noreply.github.com>
2022-01-26 17:08:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ function animate() {
  donut2.rotation.y += 0.02; // roatation along y axis
  donut2.rotation.z += 0.04; // rotation on z axis
 
- theta += Math.PI/30;
+ theta += Math.PI/100;
  
  orbit.position.x += Math.sin(theta) * 0.1;
  orbit.position.y += Math.cos(theta) * 0.1;
