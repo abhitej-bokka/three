6fcc2f98ddae235681d3afa26daa8bdb2ffef8a8
Abhitej Bokka <71954158+abhitej-bokka@users.noreply.github.com>
2022-01-26 16:35:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ scene.add(torus);
 
 
 // ORBIT DONUT
-const oradius = 2.5,otube = 1.4;
+const oradius = 1, otube = 0.56;
 const orbitGeometry = new THREE.TorusGeometry(oradius, otube, radialSegments, tubularSegments, arc);
 const orbit = new THREE.Mesh(orbitGeometry, donutMaterial);
 scene.add(orbit);
